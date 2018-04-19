7a12a3e5714d29057f8eb7ab908f3578072f9ea4
darkness198 <petrellvereen@gmail.com>
2018-04-18 22:29:36 -0400

    tests fixed

--- assets/src/components/userUtils/testcomponent.test.js
+++ assets/src/components/userUtils/testcomponent.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TestComponent from './TestComponent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<TestComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
